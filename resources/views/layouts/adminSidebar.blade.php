66e425141b89a8ab0bf9c4d0e466447c19514066
nyomanjyotisa <jyotisa1616@gmail.com>
2021-03-11 21:02:20 +0800

    pasang template admin

---
+++ resources/views/layouts/adminSidebar.blade.php
@@ -0,0 +1,31 @@
+<!-- partial:partials/_sidebar.html -->
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/documentation/documentation.html">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Transaction</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">CRUD Data</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Products</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Categories</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Courier</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
