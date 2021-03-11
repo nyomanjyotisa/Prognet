01afab3e86be63b49f453b6c55088a7921534f4c
nyomanjyotisa <jyotisa1616@gmail.com>
2021-03-11 21:23:20 +0800

    simply admin template

--- resources/views/layouts/adminSidebar.blade.php
+++ resources/views/layouts/adminSidebar.blade.php
@@ -2,13 +2,13 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="/admin">
               <i class="icon-grid menu-icon"></i>
               <span class="menu-title">Dashboard</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="pages/documentation/documentation.html">
+            <a class="nav-link" href="/admin/transaksi">
               <i class="icon-paper menu-icon"></i>
               <span class="menu-title">Transaction</span>
             </a>
